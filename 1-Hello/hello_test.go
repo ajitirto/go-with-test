ac87ea4e6a3aab78a35e003ff9cc66aca2fad193
ajitirto <ajiwoke@gmail.com>
2025-08-02 22:42:32 +0700

    add helper testing
    Signed-off-by: ajitirto <ajiwoke@gmail.com>

--- 1-Hello/hello_test.go
+++ 1-Hello/hello_test.go
@@ -6,19 +6,14 @@ func TestHello(t *testing.T) {
   t.Run("hello world", func(t *testing.T) {
     got := Hello()
     want := "Hello world"
-
-    if got != want {
-      t.Errorf("got %q want %q", got, want)
-    }
+    assertCorrectMessage(t, got, want)
   })
 
   t.Run("Hello with custome value string", func(t *testing.T) {
     got := HelloCustom("aji")
 		want := "hello, aji"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+    assertCorrectMessage(t, got, want)
+	
   })
 } 
 
