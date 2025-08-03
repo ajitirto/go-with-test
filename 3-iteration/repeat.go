76a19ad8fbf30bd41af7ce34d1dad5f85a5d96a2
ajitirto <ajiwoke@gmail.com>
2025-08-03 19:21:12 +0700

    repeat test benchmark

--- 3-iteration/repeat.go
+++ 3-iteration/repeat.go
@@ -1,9 +1,26 @@
 package iteration
 
+import "strings"
+
+// Refactor readable
+// const repeatCount = 5
+
+// func Repeat(character string) string {
+// 	var repeated string
+// 	for i:= 0; i < repeatCount ; i++ {
+// 		repeated +=  character
+// 	}
+// 	return repeated
+// }
+
+// di test benchmark lebih rungan
+
+const repeatCount = 5
+
 func Repeat(character string) string {
-	var repeated string 
-	for i:= 0; i<5 ; i++ {
-		repeated = repeated + character
+	var repeated strings.Builder
+	for i := 0; i < repeatCount; i++ {
+		repeated.WriteString(character)
 	}
-	return repeated
+	return repeated.String()
 }
